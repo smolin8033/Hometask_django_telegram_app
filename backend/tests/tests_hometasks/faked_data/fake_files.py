d76336685921af80e815fba06d28d384e7e24fe6
smoll <smolll123@mail.ru>
2022-10-31 20:01:34 +0300

    feat: write part of tests for HometaskViewSet

---
+++ backend/tests/tests_hometasks/faked_data/fake_files.py
@@ -0,0 +1,28 @@
+from io import BytesIO, StringIO
+
+from django.core.files.uploadedfile import InMemoryUploadedFile, SimpleUploadedFile
+from PIL import Image
+
+
+def generate_temp_image(counter=1):
+    images = []
+    for _ in list(range(counter)):
+        file = BytesIO()
+        image = Image.new("RGB", size=(100, 100))
+        image.save(file, "jpeg")
+        image.seek(0)
+        images.append(SimpleUploadedFile(f"{id(file)}.jpg", file.getvalue()))
+    return images[0] if counter == 1 else images
+
+
+def generate_temp_file(counter=1):
+    files = []
+    for _ in list(range(counter)):
+        file = StringIO()
+        file.write("hello world")
+        text_file = InMemoryUploadedFile(
+            file, None, f"{id(file)}.txt", "text", "charset", None
+        )
+        text_file.seek(0)
+        files.append(text_file)
+    return files[0] if counter == 1 else files
---
+++ backend/tests/tests_hometasks/test_hometask_viewset.py
@@ -0,0 +1,117 @@
+import pytest
+from django.urls import reverse
+from hometask.models import Hometask, HometaskFile, HometaskImage
+from rest_framework import status
+from tests.tests_hometasks.faked_data.fake_files import (
+    generate_temp_file,
+    generate_temp_image,
+)
+
+
+class TestHometaskViewSet:
+    @pytest.mark.django_db
+    def test_action_create_no_files(self, api_client):
+        data = {
+            "name": "test_name",
+            "start_datetime": "2022-10-30 01:55:39",
+            "end_datetime": "2022-10-30 01:55:39",
+            "url": "http://njnj.ru/golits/golits_tenses2.htm",
+            "more_info": "Additional test info",
+        }
+        url = reverse("hometasks-list")
+
+        assert Hometask.objects.count() == 0
+
+        response = api_client.post(url, data=data, format="json")
+
+        assert response.status_code == status.HTTP_201_CREATED
+        assert Hometask.objects.count() == 1
+
+        hometask = Hometask.objects.first()
+
+        hometask.start_datetime = hometask.start_datetime.strftime("%Y-%m-%d %H:%M:%S")
+        hometask.end_datetime = hometask.end_datetime.strftime("%Y-%m-%d %H:%M:%S")
+
+        assert data["name"] == hometask.name
+        assert data["start_datetime"] == hometask.start_datetime
+        assert data["end_datetime"] == hometask.end_datetime
+        assert data["url"] == hometask.url
+        assert data["more_info"] == hometask.more_info
+
+    @pytest.mark.django_db
+    def test_action_create_one_image_one_file(self, api_client):
+        images = generate_temp_image(counter=1)
+        files = generate_temp_file(counter=1)
+
+        data = {
+            "name": "test_name",
+            "start_datetime": "2022-10-30 01:55:39",
+            "end_datetime": "2022-10-30 01:55:39",
+            "url": "http://njnj.ru/golits/golits_tenses2.htm",
+            "more_info": "Additional test info",
+            "images": images,
+            "files": files,
+        }
+
+        url = reverse("hometasks-list")
+
+        assert Hometask.objects.count() == 0
+        assert HometaskImage.objects.count() == 0
+        assert HometaskFile.objects.count() == 0
+
+        response = api_client.post(url, data=data)
+
+        assert response.status_code == status.HTTP_201_CREATED
+        assert Hometask.objects.count() == 1
+        assert HometaskImage.objects.count() == 1
+        assert HometaskFile.objects.count() == 1
+
+        hometask = Hometask.objects.first()
+
+        hometask.start_datetime = hometask.start_datetime.strftime("%Y-%m-%d %H:%M:%S")
+        hometask.end_datetime = hometask.end_datetime.strftime("%Y-%m-%d %H:%M:%S")
+
+        assert data["name"] == hometask.name
+        assert data["start_datetime"] == hometask.start_datetime
+        assert data["end_datetime"] == hometask.end_datetime
+        assert data["url"] == hometask.url
+        assert data["more_info"] == hometask.more_info
+
+    @pytest.mark.django_db
+    def test_action_create_multiple_files(self, api_client):
+        images = generate_temp_image(counter=2)
+        files = generate_temp_file(counter=2)
+
+        data = {
+            "name": "test_name",
+            "start_datetime": "2022-10-30 01:55:39",
+            "end_datetime": "2022-10-30 01:55:39",
+            "url": "http://njnj.ru/golits/golits_tenses2.htm",
+            "more_info": "Additional test info",
+            "images": images,
+            "files": files,
+        }
+
+        url = reverse("hometasks-list")
+
+        assert Hometask.objects.count() == 0
+        assert HometaskImage.objects.count() == 0
+        assert HometaskFile.objects.count() == 0
+
+        response = api_client.post(url, data=data)
+
+        assert response.status_code == status.HTTP_201_CREATED
+        assert Hometask.objects.count() == 1
+        assert HometaskImage.objects.count() == 2
+        assert HometaskFile.objects.count() == 2
+
+        hometask = Hometask.objects.first()
+
+        hometask.start_datetime = hometask.start_datetime.strftime("%Y-%m-%d %H:%M:%S")
+        hometask.end_datetime = hometask.end_datetime.strftime("%Y-%m-%d %H:%M:%S")
+
+        assert data["name"] == hometask.name
+        assert data["start_datetime"] == hometask.start_datetime
+        assert data["end_datetime"] == hometask.end_datetime
+        assert data["url"] == hometask.url
+        assert data["more_info"] == hometask.more_info
